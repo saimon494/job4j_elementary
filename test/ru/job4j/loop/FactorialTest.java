47fadedfeedafa0f21fa22c5bd3a203d6b170c0e
saimon494 <saimon.494@gmail.com>
2020-09-06 09:19:06 +0300

    5.2. Создать программу, вычисляющую факториал. [#353550] - исправление

--- test/ru/job4j/loop/FactorialTest.java
+++ test/ru/job4j/loop/FactorialTest.java
@@ -7,14 +7,26 @@ import static org.junit.Assert.assertThat;
 
 public class FactorialTest {
     @Test
-    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+    public void whenFactorial5Then120() {
         int rsl = Factorial.calc(5);
         assertThat(rsl, is(120));
     }
 
     @Test
-    public void whenCalculateFactorialForZeroThenOne() {
+    public void whenFactorial0Then1() {
         int rsl = Factorial.calc(0);
         assertThat(rsl, is(1));
+    }
+
+    @Test
+    public void whenFactorial1Then1() {
+        int rsl = Factorial.calc(1);
+        assertThat(rsl, is(1));
+    }
+
+    @Test
+    public void whenFactorial3Then6() {
+        int rsl = Factorial.calc(3);
+        assertThat(rsl, is(6));
     }
 }
