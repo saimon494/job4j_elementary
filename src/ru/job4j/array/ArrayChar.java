f2d65357fe47b5b058c7912407fbeddc09b341bc
saimon494 <saimon.494@gmail.com>
2020-09-07 09:39:06 +0300

    6.4. Слово начинается с ... [#353455] - исправление

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,8 +4,9 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            for (int j = 0; j == i; j++) {
-                result = pref[j] == word[i] && pref[j + 1] == word[i + 1];
+            if (pref[i] != word[i]) {
+                result = false;
+                break;
             }
         }
         return result;
