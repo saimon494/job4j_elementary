c9ec97166476d56196a7d3e3d7bd35cdb06fa972
saimon494 <saimon.494@gmail.com>
2020-09-09 22:58:14 +0300

    3. Массивы и модели. [#353666]

---
+++ src/ru/job4j/pojo/Book.java
@@ -0,0 +1,22 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String title;
+    private int numberOfPage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumberOfPage() {
+        return numberOfPage;
+    }
+
+    public void setNumberOfPage(int numberOfPage) {
+        this.numberOfPage = numberOfPage;
+    }
+}
---
+++ src/ru/job4j/pojo/Library.java
@@ -0,0 +1,25 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        Book book2 = new Book();
+        Book book3 = new Book();
+        Book book4 = new Book();
+        Book[] books = new Book[] {book1, book2, book3, book4};
+        book1.setTitle("Clean code");
+
+        for (int index = 0; index < books.length; index++) {
+            Book b = books[index];
+            System.out.println(b.getTitle() + " - " + b.getNumberOfPage());
+        }
+        System.out.println();
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+            Book b = books[index];
+            System.out.println(b.getTitle() + " - " + b.getNumberOfPage());
+        }
+    }
+}
